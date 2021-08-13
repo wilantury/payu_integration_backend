692674559c346ebd7cf2a33c09aece79d2f87fed
Wilson Antury <ventas@antury.net>
2021-08-12 19:06:07 -0500

    [UPDATE] confirmation API

--- src/e_commerce_payu/settings.py
+++ src/e_commerce_payu/settings.py
@@ -48,11 +48,13 @@ INSTALLED_APPS = [
     'payu_payment',
     #third
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
